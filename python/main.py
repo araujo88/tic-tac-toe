6e6a13a0bbd8b2204ef048a5c579753ebb1296e0
Leonardo <46436462+araujo88@users.noreply.github.com>
2021-09-14 03:05:47 -0300

    Update main.py

--- python/main.py
+++ python/main.py
@@ -1,8 +1,9 @@
-# A simple tic-tac-toe terminal-based game. Computer input is generated through minimax algorithm.
 import TicTacToe
 
 if __name__ == "__main__":
-    tictactoe1 = TicTacToe.HumanVsRandom(plotGame=True)
-    tictactoe2 = TicTacToe.ComputerVsComputer(plotGame=True)
+    tictactoe1 = TicTacToe.HumanVsRandom(plotGame=True) # Player vs Computer (random)
     tictactoe1.run_game()
+    tictactoe2 = TicTacToe.ComputerVsComputer(plotGame=True) # Computer vs Computer (random)
     tictactoe2.run_game()
+    tictactoe3 = TicTacToe.HumanVsAI(plotGame=True) # Player vs AI (minimax algorithm)
+    tictactoe3.run_game()
