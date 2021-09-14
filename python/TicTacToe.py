ddad87e76462f3e0c66b1f9ed956e3ae6d49b217
Leonardo <46436462+araujo88@users.noreply.github.com>
2021-09-14 03:06:02 -0300

    Update TicTacToe.py

--- python/TicTacToe.py
+++ python/TicTacToe.py
@@ -1,3 +1,4 @@
+
 import numpy as np
 import copy
 import random
@@ -33,12 +34,8 @@ class TicTacToe:
     # Check who is winner
     def check_winner(self, game_tile):
         if game_tile == "x":
-            if (self._plot):
-                print("'X' wins!")
             return -1
         if game_tile == "o":
-            if (self._plot):
-                print("'O' wins!")
             return 1
 
     # Check if there is a winner
@@ -59,8 +56,6 @@ class TicTacToe:
             for j in range(3):
                 if self._game[i][j] == "_":
                     return None
-        if (self._game):
-            print("Draw!")
         return 0
 
     def player1_input(self):
@@ -92,7 +87,14 @@ class TicTacToe:
                     self.print_game()
                 else:
                     self.print_game()
-            self._turn += 1
+        if (self._plot):
+            if self._has_finished == -1:
+                print("'X' wins!")
+            if self._has_finished == 1:
+                print("'O' wins!")
+            if self._has_finished == 0:
+                print("Draw!")
+        self._turn += 1
     
     def clear_game(self):
         self._game = self._empty
@@ -108,7 +110,7 @@ class HumanVsRandom(TicTacToe):
                 elif (self._game[x][y] != "_"):
                     print("Tile already filled! Please enter another coordinate.")
                 else:
-                    self._game[x][y] = 'x'
+                    self._game[x][y] = "x"
                     break
             except ValueError:
                 print("The coordinates you entered are not valid! Please enter valid coordinates.")
@@ -118,7 +120,7 @@ class HumanVsRandom(TicTacToe):
             x = random.randint(0, 2)
             y = random.randint(0, 2)
             if self._game[x][y] == "_": # Check if tile is empty
-                self._game[x][y] = 'o'
+                self._game[x][y] = "o"
                 break
 
 class ComputerVsComputer(TicTacToe):
@@ -127,7 +129,7 @@ class ComputerVsComputer(TicTacToe):
             x = random.randint(0, 2)
             y = random.randint(0, 2)
             if self._game[x][y] == "_": # Check if tile is empty
-                self._game[x][y] = 'x'
+                self._game[x][y] = "x"
                 break
 
     def player2_input(self):
@@ -135,5 +137,123 @@ class ComputerVsComputer(TicTacToe):
             x = random.randint(0, 2)
             y = random.randint(0, 2)
             if self._game[x][y] == "_": # Check if tile is empty
-                self._game[x][y] = 'o'
+                self._game[x][y] = "o"
                 break
+
+class HumanVsAI(TicTacToe):   
+    def player1_input(self):
+        while True:
+            try:
+                x = int(input("Input x coordinate: "))
+                y = int(input("Input y coordinate: "))
+                if ((x > 2) or (x < 0) or (y < 0) or (y > 2)):
+                    print("Invalid coordinate! Please enter a value of x and y between 0 and 2.")
+                elif (self._game[x][y] != "_"):
+                    print("Tile already filled! Please enter another coordinate.")
+                else:
+                    self._game[x][y] = 'x'
+                    break
+            except ValueError:
+                print("The coordinates you entered are not valid! Please enter valid coordinates.")
+
+    def isMovesLeft(self):
+        for i in range(3) :
+            for j in range(3) :
+                if (self._game[i][j] == '_') :
+                    return True
+        return False
+
+    def minimax(self, depth, isMax):
+        score = self.check_win()
+    
+        # If Maximizer has won the game return his/her
+        # evaluated score
+        if (score == 1):
+            return score
+    
+        # If Minimizer has won the game return his/her
+        # evaluated score
+        if (score == -1):
+            return score
+    
+        # If there are no more moves and no winner then
+        # it is a tie
+        if (self.isMovesLeft() == False):
+            return 0
+    
+        # If this maximizer's move
+        if isMax:    
+            best = -1000
+    
+            # Traverse all cells
+            for i in range(3):        
+                for j in range(3):
+                
+                    # Check if cell is empty
+                    if (self._game[i][j] == "_"):
+                    
+                        # Make the move
+                        self._game[i][j] = "o"
+    
+                        # Call minimax recursively and choose
+                        # the maximum value
+                        best = max(best, self.minimax(depth + 1, not isMax))
+    
+                        # Undo the move
+                        self._game[i][j] = "_"
+            return best
+
+        # If this minimizer's move
+        else:
+            best = 1000
+    
+            # Traverse all cells
+            for i in range(3) :        
+                for j in range(3) :
+                
+                    # Check if cell is empty
+                    if (self._game[i][j] == '_') :
+                    
+                        # Make the move
+                        self._game[i][j] = "x"
+    
+                        # Call minimax recursively and choose
+                        # the minimum value
+                        best = min(best, self.minimax(depth + 1, not isMax))
+    
+                        # Undo the move
+                        self._game[i][j] = "_"
+            return best
+
+        # This will return the best possible move for the player
+    def player2_input(self):
+        bestVal = -10
+        bestMove = (-1, -1)
+    
+        # Traverse all cells, evaluate minimax function for
+        # all empty cells. And return the cell with optimal
+        # value.
+        for i in range(3):    
+            for j in range(3):
+            
+                # Check if cell is empty
+                if (self._game[i][j] == "_") :
+                
+                    # Make the move
+                    self._game[i][j] = "o"
+    
+                    # compute evaluation function for this
+                    # move.
+                    moveVal = self.minimax(0, False)
+    
+                    # Undo the move
+                    self._game[i][j] = "_"
+    
+                    # If the value of the current move is
+                    # more than the best value, then update
+                    # best/
+                    if (moveVal > bestVal) :               
+                        bestMove = (i, j)
+                        bestVal = moveVal
+
+        self._game[bestMove[0]][bestMove[1]] = "o"
