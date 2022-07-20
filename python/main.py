710e5a23014b9f487c86d5690b297375785097f6
Leonardo de Araujo <46436462+araujo88@users.noreply.github.com>
2022-07-20 00:39:28 -0300

    Delete python directory

--- python/main.py
+++
@@ -1,9 +0,0 @@
-import TicTacToe
-
-if __name__ == "__main__":
-    tictactoe1 = TicTacToe.HumanVsRandom(plotGame=True) # Player vs Computer (random)
-    tictactoe1.run_game()
-    tictactoe2 = TicTacToe.ComputerVsComputer(plotGame=True) # Computer vs Computer (random)
-    tictactoe2.run_game()
-    tictactoe3 = TicTacToe.HumanVsAI(plotGame=True) # Player vs AI (minimax algorithm)
-    tictactoe3.run_game()
